08ab1fec35ef56685de7fc473056803624be759d
Luke Rieff <lrieff@fannst.nl>
2020-08-10 18:23:34 +0200

    Basic parsing happening

--- web/mail/src/models/Email.model.ts
+++ web/mail/src/models/Email.model.ts
@@ -3,11 +3,64 @@ export interface Header {
   h_Value: string
 }
 
+export enum EmailContentType {
+  TextHTML,
+  TextPlain,
+  Base64,
+  PrintedQuotable,
+  MultipartAlternative,
+  MultipartMixed
+};
+
+export interface EmailBodySection {
+  e_Type: EmailContentType,
+  e_Content: string,
+  e_Headers: Header[],
+  e_Index: number
+};
+
+export class EmailAddress {
+  public e_Address: string;
+  public e_Name?: string;
+
+  public constructor(e_Address: string, e_Name?: string) {
+    this.e_Address = e_Address;
+    this.e_Name = e_Name;
+  }
+
+  public static parse = (raw: string): EmailAddress => {
+    let name: string = '', address: string = '';
+
+    if (raw.indexOf('@') === -1)
+      throw new Error('Could not find @');
+
+    let openBracket: number, closeBracket: number;
+    openBracket = raw.indexOf('<');
+    closeBracket = raw.indexOf('>');
+
+    if (openBracket !== -1 && closeBracket !== -1) {
+      address = raw.substring(openBracket + 1, closeBracket).trim();
+      name = raw.substring(0, openBracket).trim();
+    } else if (
+      openBracket !== -1 && closeBracket === -1 ||
+      openBracket === -1 && closeBracket !== -1
+    ) {
+      throw new Error('Only one bracket found, address should contain two.');
+    } else address = raw.trim();
+
+    return new EmailAddress(address, name);
+  };
+};
+
 export class Email {
   public e_Headers: Header[];
+  public e_Sections: EmailBodySection[];
+  public e_Subject: string;
 
   public constructor()
   {
     this.e_Headers = [];
+    this.e_Sections = [];
+    this.e_Subject = '';
   }
 };
--- web/mail/src/parsers/MIMEParser.parser.ts
+++ web/mail/src/parsers/MIMEParser.parser.ts
@@ -1,17 +1,17 @@
-import { Email, Header } from '../models/Email.model';
+import { Email, Header, EmailAddress } from '../models/Email.model';
 import { stringify } from 'querystring';
 
 const parseHeaders = (raw: string): Header[] => {
   let result: Header[] = [];
   
-  raw.split('\r\n').forEach(rawHeader => {
+  uniteLines(raw).split('\r\n').forEach(rawHeader => {
     if (rawHeader.trim() === '') return;
 
     let sepIndex: number = rawHeader.indexOf(':');
     if (sepIndex === -1) throw new Error('Invalid headers');
 
     let header: Header = {
-      h_Key: rawHeader.substring(0, sepIndex).trim(),
+      h_Key: rawHeader.substring(0, sepIndex).trim().toLowerCase(),
       h_Value: rawHeader.substr(++sepIndex).trim()
     };
 
@@ -45,22 +45,37 @@ const splitHeadersAndBody = (raw: string): { headers: string, body: string } => 
 const uniteLines = (raw: string): string => {
   let result: string = '';
 
-  return stringify;
+  const lines: string[] = raw.split('\r\n');
+  for (let i: number = 0; i < lines.length; i++) {
+    if (i+1 < lines.length && lines[i+1][0] === ' ') {
+      if (result[result.length-1] === ';') result += ' ';
+      result += lines[i].trim();
+    } else {
+      result += lines[i].trim();
+      result += '\r\n';
+    }
+  }
+
+  return result;
+};
+
+const recursiveParse = (raw: string, target: Email, i: number) => {
+  const { headers, body } = splitHeadersAndBody(raw);
+  const parsedHeaders: Header[] = parseHeaders(headers);
+
+  parsedHeaders.forEach(header => {
+    if (header.h_Key === 'from') console.log(EmailAddress.parse(header.h_Value));
+  });
 };
 
 export const parse = (raw: string): Email => {
   let result: Email = new Email();
   let start: number = Date.now();
 
-  const cleaned: string = raw.replace(/ +(?= )/g,'');
+  const cleaned: string = raw.replace(/ +(?= )/g, '').replace('\t', ' ');
 
-  // Splits the headers and the body, and starts the parsing process
-  //  this will be an different recursive function
-  const { headers, body } = splitHeadersAndBody(cleaned);
-  result.e_Headers = parseHeaders(headers);
+  recursiveParse(cleaned, result, 0);
 
-
-  let end: number = Date.now();
-  console.log(`Parsing finished in ${end-start}ms`)
+  console.log(`Parsing finished in ${Date.now() - start}ms`)
   return result;
 };
